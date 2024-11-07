dbb48b817ad3bedbb0094dc472ba34f87b653c27
Wesley <ochoka2018@gmail.com>
2024-11-07 14:34:49 +0300

    Created the codes to access kobo FGD data and allow user to either view and download the formatted data or Review, make edits and download the edited version of the data

--- elmi_app1.py
+++ elmi_app1.py
@@ -145,6 +145,92 @@ if st.session_state['authentication_status']:
                     st.write(f"No data available for {kii_selected}")
                     pass
 
+        # Handling FGD data
+        if data_type == "FGD":
+
+            # Extract KII data only and allow user to select one
+            fgds = [name for name in project_names if 'fgd' in name.lower()]
+
+            # Let user choose a KII from the list
+            fgd_selected = st.sidebar.selectbox(
+                f"{data_type} Verifications", fgds, index=None,
+                placeholder="Choose a Verification")
+
+            # Get the data
+            # Find the index of the selected FGD
+            if fgd_selected is not None:
+                index_fgd_selected = project_names.index(fgd_selected)
+                user_id_selected = user_ids[index_fgd_selected]
+
+                dfs = ka.generate_dataframes(user_id_selected)
+
+                if dfs is not None:
+                    ver_fgd = dfs.keys()
+
+                    # Select what to do with the file
+                    fgd_select1 = st.sidebar.selectbox(
+                        "Select Verification",
+                        ver_fgd, index=None)
+
+                    # Select what to do with the file
+                    if fgd_select1 is not None:
+                        action_fgd = st.sidebar.selectbox("What do you want to"
+                                                          " do with the data?",
+                                                          ("View", "Review"),
+                                                          index=None)
+                        df = dfs[fgd_select1]
+
+                        # Remove column names
+                        if action_fgd is not None:
+                            if action_fgd == "View":
+                                st.subheader(f"{fgd_select1}")
+                                df.columns = [''] * len(df.columns)
+                                st.markdown(df.to_html(escape=False),
+                                            unsafe_allow_html=True)
+                                # Create downloadable excel
+                                output = ka.create_excel_file(
+                                    df,
+                                    sheet_name=fgd_select1)
+
+                                # Download button for the Excel file
+                                st.sidebar.download_button(
+                                    label="Download data as Excel",
+                                    data=output,
+                                    file_name=f"{fgd_select1} data.xlsx",
+                                    mime="application/vnd.openxmlformats-officedocument"
+                                         ".spreadsheetml.sheet"
+                                )
+                            if action_fgd == "Review":
+                                st.subheader(f"{fgd_select1}")
+                                edited_df = ka.editable_dataframe(
+                                    df,
+                                    sheet_name=fgd_select1)
+                                # Check if new_df has been created successfully
+                                # before calling to_html
+                                if edited_df is not None:
+                                    # Create downloadable excel
+                                    edited_df.columns = [''] * len(edited_df.columns)
+                                    output = ka.create_excel_file(
+                                    edited_df,
+                                    sheet_name=fgd_select1)
+
+                                    # Download button for the Excel file
+                                    st.sidebar.download_button(
+                                        label="Download data as Excel",
+                                        data=output,
+                                        file_name=f"{fgd_select1} Reviewed Data.xlsx",
+                                        mime="application/vnd.openxmlformats-officedocument"
+                                             ".spreadsheetml.sheet"
+                                    )
+                                else:
+                                    st.warning(
+                                        "Please commit changes to get "
+                                        "the updated DataFrame.")
+
+                else:
+                    st.write(f"No data available for {fgd_selected}")
+                    pass
+
     authenticator.logout("Logout", "sidebar")
 elif st.session_state['authentication_status'] is False:
     st.error('Username/password is incorrect')
