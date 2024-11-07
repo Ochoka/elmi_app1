3f15ba1f8fe00265c1aebb40042b57b18543ff1b
Wesley <ochoka2018@gmail.com>
2024-11-07 14:13:26 +0300

    - Imported essential libraries: streamlit, streamlit_authenticator, and yaml - Configured Streamlit page settings (title, icon, layout) - Loaded and parsed config.yaml for authentication settings - Initialized user authentication with streamlit_authenticator -Implemented login functionality with error handling in the sidebar - Added conditional authentication status check - Customized sidebar and main content background color with inline CSS - Displayed personalized greeting in sidebar based on login - Added logout widget

---
+++ elmi_app1.py
@@ -0,0 +1,58 @@
+# Import needed libraries
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+# Page configuration
+st.set_page_config(page_title="Data Handling", page_icon=":bar_chart:",
+                   layout="wide")
+
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+# Pre-hashing all plain text passwords once
+# stauth.Hasher.hash_passwords(config['credentials'])
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
+
+try:
+    authenticator.login()
+except Exception as e:
+    st.sidebar.error(e)
+
+if st.session_state['authentication_status']:
+    # Use Streamlit's markdown feature to insert HTML with inline CSS
+    st.html(
+        """
+    <style>
+    [data-testid="stSidebarContent"] {
+        color: Black;
+        background-color: #03fcf8;
+    }
+
+    [data-testid="stMain"] {
+        color: Black;
+        background-color: #cce7e8;
+    }
+
+
+
+    </style>
+    """
+    )
+    st.sidebar.write(f'Welcome **{st.session_state["name"]}**')
+    # set title
+    st.title("Download, Format and Edit Data")
+
+
+    authenticator.logout("Logout", "sidebar")
+elif st.session_state['authentication_status'] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state['authentication_status'] is None:
+    st.warning('Please enter your username and password')
